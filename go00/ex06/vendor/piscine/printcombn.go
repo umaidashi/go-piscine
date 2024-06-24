069e35835393af91919d0fce930cb7d7eb8a8edd
umaidashi <umaidashi18@gmail.com>
2024-06-24 22:06:31 +0900

    wip

--- go00/ex06/vendor/piscine/printcombn.go
+++ go00/ex06/vendor/piscine/printcombn.go
@@ -6,8 +6,16 @@ func PrintCombN(nb int) {
 	if nb < 1 || nb > 9 {
 		return
 	}
-	var digits[nb] rune
+	digits := [10]rune{}
 	for i := 0; i <= nb; i++ {
+		digits[i] = rune('0' + i)
+	}
+	printDigits(digits, nb)
+	ft.PrintRune('\n')
+}
 
+func printDigits(digits [10]rune, nb int) {
+	for i := 0; i < nb; i++ {
+		ft.PrintRune(digits[i])
 	}
 }
