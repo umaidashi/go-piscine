476d26b46c3b465164fb0750641ae52e75fa0814
umaidashi <umaidashi18@gmail.com>
2024-06-24 21:54:46 +0900

    wip

---
+++ go00/ex06/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "piscine"
+
+func main() {
+	piscine.PrintCombN(0)
+	piscine.PrintCombN(1)
+	piscine.PrintCombN(3)
+	piscine.PrintCombN(9)
+	piscine.PrintCombN(10)
+}
---
+++ go00/ex06/vendor/piscine/printcombn.go
@@ -0,0 +1,13 @@
+package piscine
+
+import "ft"
+
+func PrintCombN(nb int) {
+	if nb < 1 || nb > 9 {
+		return
+	}
+	var digits[nb] rune
+	for i := 0; i <= nb; i++ {
+
+	}
+}
