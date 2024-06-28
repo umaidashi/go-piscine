c1e443f516954e1d98b82e5ba1ee081ca158e2ab
umaidashi <umaidashi18@gmail.com>
2024-06-28 21:03:35 +0900

    feat: ex06

--- go00/ex06/vendor/piscine/printcombn.go
+++ go00/ex06/vendor/piscine/printcombn.go
@@ -1,6 +1,9 @@
 package piscine
 
-import "ft"
+import (
+	"ft"
+)
+
 
 func PrintCombN(nb int) {
 	if nb < 1 || nb > 9 {
@@ -10,8 +13,24 @@ func PrintCombN(nb int) {
 	for i := 0; i <= nb; i++ {
 		digits[i] = rune('0' + i)
 	}
-	printDigits(digits, nb)
+	printCombNRec(digits, nb, 0, 0)
 	ft.PrintRune('\n')
+}
+
+// fmt.Println("digits:", digits, "nb:", nb, "index:", index, "start:", start)
+func printCombNRec(digits [10]rune, nb, index, start int) {
+	if index == nb {
+		printDigits(digits, nb)
+		if digits[0] != rune('9'-nb+1) {
+			ft.PrintRune(',')
+			ft.PrintRune(' ')
+		}
+		return
+	}
+	for i := start; i <= 9; i++ {
+		digits[index] = rune('0' + i)
+		printCombNRec(digits, nb, index+1, i+1)
+	}
 }
 
 func printDigits(digits [10]rune, nb int) {
