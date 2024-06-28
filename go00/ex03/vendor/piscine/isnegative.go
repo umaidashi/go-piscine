e58554595f6684d40e4e0961441716314bbbb19b
umaidashi <umaidashi18@gmail.com>
2024-06-28 21:15:58 +0900

    feat: add nl

--- go00/ex03/vendor/piscine/isnegative.go
+++ go00/ex03/vendor/piscine/isnegative.go
@@ -3,7 +3,7 @@ package piscine
 import "ft"
 
 func IsNegative(nb int) {
-	if (nb >= 0) {
+	if nb >= 0 {
 		ft.PrintRune('F')
 	} else {
 		ft.PrintRune('T')
--- go00/ex04/vendor/piscine/printcomb.go
+++ go00/ex04/vendor/piscine/printcomb.go
@@ -7,18 +7,18 @@ func PrintComb() {
 	for i := '0'; i <= '7'; i++ {
 		for j := i + 1; j <= '8'; j++ {
 			for k := j + 1; k <= '9'; k++ {
-				if (i != j && j != k && i != k) {
+				if i != j && j != k && i != k {
 					if !first {
 						ft.PrintRune(',')
 						ft.PrintRune(' ')
 					}
-					first = false;
+					first = false
 					ft.PrintRune(i)
 					ft.PrintRune(j)
 					ft.PrintRune(k)
 				}
 			}
 		}
-	} 
+	}
 	ft.PrintRune('\n')
 }
